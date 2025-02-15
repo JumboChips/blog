c655a58059a0a2fe0b921c213e9955bf59f05103
jumbochips <jumbochips1@gmail.com>
2025-02-16 03:15:59 +0900

    cors 설정 변경

--- blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.jumbochips.poml_jpa.common.auth.config;
 
-import com.jumbochips.poml_jpa.common.jwt.JwtFilter;
-import com.jumbochips.poml_jpa.common.jwt.JwtUtil;
-import com.jumbochips.poml_jpa.common.jwt.LoginFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,86 +16,96 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Collections;
-import java.util.Arrays;
+import com.jumbochips.poml_jpa.common.jwt.JwtFilter;
+import com.jumbochips.poml_jpa.common.jwt.JwtUtil;
+import com.jumbochips.poml_jpa.common.jwt.LoginFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final JwtUtil jwtUtil;
+        private final AuthenticationConfiguration authenticationConfiguration;
+        private final JwtUtil jwtUtil;
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+                return configuration.getAuthenticationManager();
+        }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http,
+                        AuthenticationConfiguration authenticationConfiguration) throws Exception {
 
-        http
-                .cors((cors) -> cors
-                        .configurationSource(new CorsConfigurationSource() {
+                http
+                                .cors((cors) -> cors
+                                                .configurationSource(new CorsConfigurationSource() {
 
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration config = new CorsConfiguration();
+                                                        @Override
+                                                        public CorsConfiguration getCorsConfiguration(
+                                                                        HttpServletRequest request) {
+                                                                CorsConfiguration config = new CorsConfiguration();
 
-                                config.setAllowCredentials(true);
-                                config.addAllowedOriginPattern("*");
-				                config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://jumbochips.com", "https://jumbochips.com"));
-                                config.setAllowedMethods(Collections.singletonList("*"));
-                                config.setAllowedHeaders(Collections.singletonList("*"));
-                                config.setAllowCredentials(true);
-                                config.setMaxAge(3600L);
+                                                                config.setAllowCredentials(true);
+                                                                config.addAllowedOriginPattern("*");
+                                                                config.setAllowedOrigins(Arrays.asList(
+                                                                                "http://localhost:3000",
+                                                                                "https://jumbochips.com"));
+                                                                config.setAllowedMethods(
+                                                                                Collections.singletonList("*"));
+                                                                config.setAllowedHeaders(
+                                                                                Collections.singletonList("*"));
+                                                                config.setAllowCredentials(true);
+                                                                config.setMaxAge(3600L);
 
-                                config.setExposedHeaders(Collections.singletonList("Authorization"));
+                                                                config.setExposedHeaders(Collections
+                                                                                .singletonList("Authorization"));
 
-                                return config;
-                            }
-                        }));
+                                                                return config;
+                                                        }
+                                                }));
 
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
+                // csrf disable
+                http
+                                .csrf((auth) -> auth.disable());
 
-        //Form 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
+                // Form 로그인 방식 disable
+                http
+                                .formLogin((auth) -> auth.disable());
 
-        //http basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
+                // http basic 인증 방식 disable
+                http
+                                .httpBasic((auth) -> auth.disable());
 
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/api/v1/**", "/api/v1/join", "/uploads/**").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                );
+                // 경로별 인가 작업
+                http
+                                .authorizeHttpRequests((auth) -> auth
+                                                .requestMatchers("/login", "/api/v1/**", "/api/v1/join", "/uploads/**")
+                                                .permitAll()
+                                                .requestMatchers("/admin").hasRole("ADMIN")
+                                                .anyRequest().authenticated());
 
-        http
-                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+                http
+                                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
 
-        http
-                .addFilterAt(new LoginFilter(
-                        authenticationManager(authenticationConfiguration), jwtUtil),
-                        UsernamePasswordAuthenticationFilter.class);
+                http
+                                .addFilterAt(new LoginFilter(
+                                                authenticationManager(authenticationConfiguration), jwtUtil),
+                                                UsernamePasswordAuthenticationFilter.class);
 
+                // 세션 설정
+                http
+                                .sessionManagement((session) -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        //세션 설정
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
-    }
+                return http.build();
+        }
 }
