3d4f6187802e8d6a72d8e73e1702006043579dd2
jumbochips <jumbochips1@gmail.com>
2025-02-25 19:01:50 +0900

    프로필 이미지 경로 최종 수정

--- blog_fe/pages/index.vue
+++ blog_fe/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Introduce></Introduce>
+  <Introduce :image-url="'/profilePhoto.jpeg'"></Introduce>
 </template>
 <script setup lang="ts">
 import Introduce from '@/components/introduce.vue';
