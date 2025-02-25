1c9a6ced1460e05d6c9060a44d040313dead3626
jumbochips <jumbochips1@gmail.com>
2025-02-25 11:44:31 +0900

    debug log

--- blog_be/src/main/java/com/jumbochips/poml_jpa/project/controller/ProjectController.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/project/controller/ProjectController.java
@@ -75,6 +75,7 @@ public class ProjectController {
             ProjectResponseDto project = projectService.updateProject(projectId, projectRequestDto);
             return ResponseEntity.ok(project);
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.badRequest().build();
         }
     }
