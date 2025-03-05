2235301e2e687b3eb77cab0683ea9cdf51683225
jumbochips <jumbochips1@gmail.com>
2025-03-05 16:22:53 +0900

    직렬화 추가

--- blog_be/src/main/java/com/jumbochips/poml_jpa/blog/dto/BlogResponseDto.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/blog/dto/BlogResponseDto.java
@@ -1,6 +1,10 @@
 package com.jumbochips.poml_jpa.blog.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,8 +24,12 @@ public class BlogResponseDto {
     private String title;
     private String content;
     private List<Long> tagIds; // 태그 ID 리스트
+    @JsonSerialize(using = LocalTimeSerializer.class)
+    @JsonDeserialize(using = LocalTimeDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
+    @JsonSerialize(using = LocalTimeSerializer.class)
+    @JsonDeserialize(using = LocalTimeDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt;
 
