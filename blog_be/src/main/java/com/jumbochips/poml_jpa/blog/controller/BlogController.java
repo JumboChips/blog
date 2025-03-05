ba53c1de2044c504ee053e29c5800dce392107b3
jumbochips <jumbochips1@gmail.com>
2025-03-05 18:07:40 +0900

    jackson 설정 application.yml 추가 및 blogController log 추가

--- blog_be/src/main/java/com/jumbochips/poml_jpa/blog/controller/BlogController.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/blog/controller/BlogController.java
@@ -1,5 +1,6 @@
 package com.jumbochips.poml_jpa.blog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jumbochips.poml_jpa.blog.domain.BlogCategory;
 import com.jumbochips.poml_jpa.blog.domain.BlogTag;
 import com.jumbochips.poml_jpa.blog.dto.BlogRequestDto;
@@ -35,8 +36,13 @@ public class BlogController {
     public ResponseEntity<BlogResponseDto> getBlogById(
             @PathVariable Long blogId
     ){
+        ObjectMapper objectMapper = new ObjectMapper();
+
         try {
+
             BlogResponseDto blogResponseDto = blogService.getBlog(blogId);
+            String json = objectMapper.writeValueAsString(blogResponseDto);
+            System.out.println("Json 반환 결과" + json);
             return ResponseEntity.ok(blogResponseDto);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
