cc21d3c334e62102d63634d814de79da347338f1
jumbochips <jumbochips1@gmail.com>
2025-04-01 03:21:05 +0900

    refactor: comment 관련 api 경로 수정 및 security config /error 인가 설정

--- blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                 // 경로별 인가 작업
                 http
                                 .authorizeHttpRequests((auth) -> auth
-                                                .requestMatchers("/login", "/api/v1/**", "/api/v1/join", "/uploads/**")
+                                                .requestMatchers("/login", "/api/v1/**", "/api/v1/join", "/uploads/**", "/error")
                                                 .permitAll()
                                                 .requestMatchers("/admin").hasRole("ADMIN")
                                                 .anyRequest().authenticated());
