3386362d83a8692763984bfbcf6f81082868cbf4
jumbochips <jumbochips1@gmail.com>
2025-04-02 20:56:18 +0900

    refactor: application-prod time zone utc로 변경

--- blog_be/src/main/java/com/jumbochips/poml_jpa/comment/dto/CommentResponseDto.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/comment/dto/CommentResponseDto.java
@@ -3,11 +3,13 @@ package com.jumbochips.poml_jpa.comment.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.Instant;
+
 @Builder
 @Data
 public class CommentResponseDto {
     private Long commentId;
     private String username;
     private String content;
-    private String createdAt;
+    private Instant createdAt;
 }
