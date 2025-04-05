4608ae1646328d1865c3b1c6e1684bb8ab9d08a5
jumbochips <jumbochips1@gmail.com>
2025-04-05 17:31:13 +0900

    refactor: login 기능 리팩토링 오류 수정2

--- blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
+++ blog_be/src/main/java/com/jumbochips/poml_jpa/common/auth/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import com.jumbochips.poml_jpa.common.auth.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,13 +37,22 @@ public class SecurityConfig {
 
         @Bean
         public AuthenticationManager authenticationManager() throws Exception {
-                return authConfig.getAuthenticationManager();
+                return new ProviderManager(authenticationProvider());
         }
 
         @Bean
         public BCryptPasswordEncoder bCryptPasswordEncoder() {
                 return new BCryptPasswordEncoder();
         }
+
+        @Bean
+        public DaoAuthenticationProvider authenticationProvider() {
+                DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+                provider.setUserDetailsService(userDetailsService);
+                provider.setPasswordEncoder(bCryptPasswordEncoder());
+                return provider;
+        }
+
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
